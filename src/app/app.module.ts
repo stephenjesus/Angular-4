97e55ac133bf2833dc30f975b4d32e891a83a881
stephenjesus <stephenprakash5@gmail.com>
2017-08-05 10:23:43 +0530

    searchfrom DB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
+import {HttpModule} from '@angular/http'
+import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
